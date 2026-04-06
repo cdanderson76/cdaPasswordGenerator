34116e749f547276f7f909b41cc277ca26a1e1b5
Cliff Anderson <snappinmambo@gmail.com>
2026-04-05 20:09:41 -0500

    completed password by rendering the characters in each field

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //START CODE BELOW
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"]
+  const btnEl = document.getElementById('btn')
+
+  const outputOneEl = document.getElementById('outputOne')
+  const outputTwoEl = document.getElementById('outputTwo')
+
+  const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+  "/"]
 
 //GENERATE PASSWORD
 
-function generatePassword() {
+btnEl.addEventListener('click', function() {
 
-  const passwordArray = [];
+  const passwordArrayOne = [];
+  const passwordArrayTwo = [];
 
   for( i = 0; i < 15; i++ ) {
 
-    const randNum = Math.floor(Math.random() * characters.length);
-    passwordArray.push(characters[randNum]);
-  }
-}
+    const randNum = Math.floor(Math.random() * characters.length)
+    const randNumTwo = Math.floor(Math.random() * characters.length)
 
-generatePassword()
+    passwordArrayOne.push(characters[randNum])
+    passwordArrayTwo.push(characters[randNumTwo])
+
+    outputOneEl.textContent = passwordArrayOne.join('')
+    outputTwoEl.textContent = passwordArrayTwo.join('')
+    console.log(passwordArrayOne)
+  }
+})
+
 
 })
